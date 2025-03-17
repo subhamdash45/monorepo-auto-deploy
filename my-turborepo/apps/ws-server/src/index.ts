ee725050584cfcfd5bf2d130038a9f7b74958e5e
subhamdash45 <mrsubhamdash1998@gmail.com>
2025-03-17 10:08:13 +0530

    added develop branch

--- my-turborepo/apps/ws-server/src/index.ts
+++ my-turborepo/apps/ws-server/src/index.ts
@@ -2,15 +2,15 @@ import { WebSocketServer } from "ws";
 import { client } from "@repo/db/client";
 
 const server = new WebSocketServer({
-  port: 3001
+    port: 3001
 });
 
 server.on("connection", async (socket) => {
-  client.user.create({
-    data: {
-      username: Math.random().toString(),
-      password: Math.random().toString()
-    }
-  })
-  socket.send("Hi there you are connected to the server");
+    client.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString()
+        }
+    })
+    socket.send("Hi there you are connected to the server");
 })
